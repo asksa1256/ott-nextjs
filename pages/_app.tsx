b1db52c0779e8016fd7013d76c71999d0663cc01
leesangdal <asksa1256@gmail.com>
2025-06-30 18:16:12 +0900

    app에 공통 컴포넌트(Header, Container) 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import type { AppProps } from "next/app";
 import React from "react";
+import type { AppProps } from "next/app";
+import Header from "@/components/Header";
+import Container from "@/components/Container";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Container>
+        <Component {...pageProps} />
+      </Container>
+    </>
+  );
 }
